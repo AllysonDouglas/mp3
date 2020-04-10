d28f4555fdbc7d037474758278be1508e56d36f6
AllysonDouglas <atdo227@uky.edu>
2020-04-10 17:55:10 -0400

    Revert "q3"
    This reverts commit 5ba69c0088528b1d607e20d83dc17d8ef7a351e7.

--- mp3/jenkins/core/src/main/java/jenkins/model/Jenkins.java
+++ mp3/jenkins/core/src/main/java/jenkins/model/Jenkins.java
@@ -4133,10 +4133,6 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
             return null;
         }
     }
-	public static void updateAndTrim() {
-		updateComputerList();
-		trimLabels();
-	}
 
     /**
      * Hash of {@link #VERSION}.
