f32ba77c51d9357ae460106392234b730d8fc2e0
AllysonDouglas <atdo227@uky.edu>
2020-04-10 23:59:59 -0400

    fix typos

--- mp3/jenkins/test/src/test/java/jenkins/model/IdFromFilenameTest.java
+++ mp3/jenkins/test/src/test/java/jenkins/model/IdFromFilenameTest.java
@@ -1,10 +1,7 @@
 package jenkins.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 class IdFromFilenameTest {
 
