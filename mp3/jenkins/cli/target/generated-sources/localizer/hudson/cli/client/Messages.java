e50a005871ce32cc0a860678866d82dad14f93fd
AllysonDouglas <atdo227@uky.edu>
2020-04-10 20:29:52 -0500

    q5

--- mp3/jenkins/cli/target/generated-sources/localizer/hudson/cli/client/Messages.java
+++ mp3/jenkins/cli/target/generated-sources/localizer/hudson/cli/client/Messages.java
@@ -24,124 +24,6 @@ public class Messages {
     private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
 
     /**
-     * Key {@code CLI.Usage}: {@code Jenkins CLI
-     * Usage: java -jar
-     * jenkins-cli.jar [-s URL] command [opts...] args...
-     * Options:
-     * -s URL    
-     * : the server URL (defaults to the JENKINS_URL env var)
-     * -i KEY       :
-     * SSH private key file used for authentication
-     * -p HOST:PORT : HTTP proxy
-     * host and port for HTTPS proxy tunneling. See
-     * http://jenkins-ci.org/https-proxy-tunnel
-     * -noCertificateCheck : bypass
-     * HTTPS certificate check entirely. Use with caution
-     * -noKeyAuth   :
-     * don't try to load the SSH authentication private key. Conflicts with
-     * -i
-     * 
-     * The available commands depend on the server. Run the 'help'
-     * command to
-     * see the list.}.
-     * 
-     * @return
-     *     {@code Jenkins CLI
-     *     Usage: java -jar jenkins-cli.jar [-s URL] command
-     *     [opts...] args...
-     *     Options:
-     *     -s URL       : the server URL (defaults to
-     *     the JENKINS_URL env var)
-     *     -i KEY       : SSH private key file used for
-     *     authentication
-     *     -p HOST:PORT : HTTP proxy host and port for HTTPS proxy
-     *     tunneling. See
-     *     http://jenkins-ci.org/https-proxy-tunnel
-     *     -noCertificateCheck : bypass
-     *     HTTPS certificate check entirely. Use with caution
-     *     -noKeyAuth   :
-     *     don't try to load the SSH authentication private key. Conflicts with
-     *     -i
-     *     
-     *     The available commands depend on the server. Run the 'help'
-     *     command to
-     *     see the list.}
-     */
-    public static String CLI_Usage() {
-        return holder.format("CLI.Usage");
-    }
-
-    /**
-     * Key {@code CLI.Usage}: {@code Jenkins CLI
-     * Usage: java -jar
-     * jenkins-cli.jar [-s URL] command [opts...] args...
-     * Options:
-     * -s URL    
-     * : the server URL (defaults to the JENKINS_URL env var)
-     * -i KEY       :
-     * SSH private key file used for authentication
-     * -p HOST:PORT : HTTP proxy
-     * host and port for HTTPS proxy tunneling. See
-     * http://jenkins-ci.org/https-proxy-tunnel
-     * -noCertificateCheck : bypass
-     * HTTPS certificate check entirely. Use with caution
-     * -noKeyAuth   :
-     * don't try to load the SSH authentication private key. Conflicts with
-     * -i
-     * 
-     * The available commands depend on the server. Run the 'help'
-     * command to
-     * see the list.}.
-     * 
-     * @return
-     *     {@code Jenkins CLI
-     *     Usage: java -jar jenkins-cli.jar [-s URL] command
-     *     [opts...] args...
-     *     Options:
-     *     -s URL       : the server URL (defaults to
-     *     the JENKINS_URL env var)
-     *     -i KEY       : SSH private key file used for
-     *     authentication
-     *     -p HOST:PORT : HTTP proxy host and port for HTTPS proxy
-     *     tunneling. See
-     *     http://jenkins-ci.org/https-proxy-tunnel
-     *     -noCertificateCheck : bypass
-     *     HTTPS certificate check entirely. Use with caution
-     *     -noKeyAuth   :
-     *     don't try to load the SSH authentication private key. Conflicts with
-     *     -i
-     *     
-     *     The available commands depend on the server. Run the 'help'
-     *     command to
-     *     see the list.}
-     */
-    public static Localizable _CLI_Usage() {
-        return new Localizable(holder, "CLI.Usage");
-    }
-
-    /**
-     * Key {@code CLI.NoURL}: {@code Neither -s nor the JENKINS_URL env var
-     * is specified.}.
-     * 
-     * @return
-     *     {@code Neither -s nor the JENKINS_URL env var is specified.}
-     */
-    public static String CLI_NoURL() {
-        return holder.format("CLI.NoURL");
-    }
-
-    /**
-     * Key {@code CLI.NoURL}: {@code Neither -s nor the JENKINS_URL env var
-     * is specified.}.
-     * 
-     * @return
-     *     {@code Neither -s nor the JENKINS_URL env var is specified.}
-     */
-    public static Localizable _CLI_NoURL() {
-        return new Localizable(holder, "CLI.NoURL");
-    }
-
-    /**
      * Key {@code CLI.NoSuchFileExists}: {@code No such file exists: {0}}.
      * 
      * @param arg0
@@ -166,11 +48,11 @@ public class Messages {
     }
 
     /**
-     * Key {@code CLI.VersionMismatch}: {@code Version mismatch. This CLI
+     * Key {@code CLI.VersionMismatch}: {@code Version mismatch. This CLI
      * cannot work with this Jenkins server.}.
      * 
      * @return
-     *     {@code Version mismatch. This CLI cannot work with this Jenkins
+     *     {@code Version mismatch. This CLI cannot work with this Jenkins
      *     server.}
      */
     public static String CLI_VersionMismatch() {
@@ -178,15 +60,133 @@ public class Messages {
     }
 
     /**
-     * Key {@code CLI.VersionMismatch}: {@code Version mismatch. This CLI
+     * Key {@code CLI.VersionMismatch}: {@code Version mismatch. This CLI
      * cannot work with this Jenkins server.}.
      * 
      * @return
-     *     {@code Version mismatch. This CLI cannot work with this Jenkins
+     *     {@code Version mismatch. This CLI cannot work with this Jenkins
      *     server.}
      */
     public static Localizable _CLI_VersionMismatch() {
         return new Localizable(holder, "CLI.VersionMismatch");
+    }
+
+    /**
+     * Key {@code CLI.NoURL}: {@code Neither -s nor the JENKINS_URL env var
+     * is specified.}.
+     * 
+     * @return
+     *     {@code Neither -s nor the JENKINS_URL env var is specified.}
+     */
+    public static String CLI_NoURL() {
+        return holder.format("CLI.NoURL");
+    }
+
+    /**
+     * Key {@code CLI.NoURL}: {@code Neither -s nor the JENKINS_URL env var
+     * is specified.}.
+     * 
+     * @return
+     *     {@code Neither -s nor the JENKINS_URL env var is specified.}
+     */
+    public static Localizable _CLI_NoURL() {
+        return new Localizable(holder, "CLI.NoURL");
+    }
+
+    /**
+     * Key {@code CLI.Usage}: {@code Jenkins CLI
+     * Usage: java -jar
+     * jenkins-cli.jar [-s URL] command [opts...] args...
+     * Options:
+     * -s URL    
+     * : the server URL (defaults to the JENKINS_URL env var)
+     * -i KEY       :
+     * SSH private key file used for authentication
+     * -p HOST:PORT : HTTP proxy
+     * host and port for HTTPS proxy tunneling. See
+     * http://jenkins-ci.org/https-proxy-tunnel
+     * -noCertificateCheck : bypass
+     * HTTPS certificate check entirely. Use with caution
+     * -noKeyAuth   :
+     * don't try to load the SSH authentication private key. Conflicts with
+     * -i
+     * 
+     * The available commands depend on the server. Run the 'help'
+     * command to
+     * see the list.}.
+     * 
+     * @return
+     *     {@code Jenkins CLI
+     *     Usage: java -jar jenkins-cli.jar [-s URL] command
+     *     [opts...] args...
+     *     Options:
+     *     -s URL       : the server URL (defaults to
+     *     the JENKINS_URL env var)
+     *     -i KEY       : SSH private key file used for
+     *     authentication
+     *     -p HOST:PORT : HTTP proxy host and port for HTTPS proxy
+     *     tunneling. See
+     *     http://jenkins-ci.org/https-proxy-tunnel
+     *     -noCertificateCheck : bypass
+     *     HTTPS certificate check entirely. Use with caution
+     *     -noKeyAuth   :
+     *     don't try to load the SSH authentication private key. Conflicts with
+     *     -i
+     *     
+     *     The available commands depend on the server. Run the 'help'
+     *     command to
+     *     see the list.}
+     */
+    public static String CLI_Usage() {
+        return holder.format("CLI.Usage");
+    }
+
+    /**
+     * Key {@code CLI.Usage}: {@code Jenkins CLI
+     * Usage: java -jar
+     * jenkins-cli.jar [-s URL] command [opts...] args...
+     * Options:
+     * -s URL    
+     * : the server URL (defaults to the JENKINS_URL env var)
+     * -i KEY       :
+     * SSH private key file used for authentication
+     * -p HOST:PORT : HTTP proxy
+     * host and port for HTTPS proxy tunneling. See
+     * http://jenkins-ci.org/https-proxy-tunnel
+     * -noCertificateCheck : bypass
+     * HTTPS certificate check entirely. Use with caution
+     * -noKeyAuth   :
+     * don't try to load the SSH authentication private key. Conflicts with
+     * -i
+     * 
+     * The available commands depend on the server. Run the 'help'
+     * command to
+     * see the list.}.
+     * 
+     * @return
+     *     {@code Jenkins CLI
+     *     Usage: java -jar jenkins-cli.jar [-s URL] command
+     *     [opts...] args...
+     *     Options:
+     *     -s URL       : the server URL (defaults to
+     *     the JENKINS_URL env var)
+     *     -i KEY       : SSH private key file used for
+     *     authentication
+     *     -p HOST:PORT : HTTP proxy host and port for HTTPS proxy
+     *     tunneling. See
+     *     http://jenkins-ci.org/https-proxy-tunnel
+     *     -noCertificateCheck : bypass
+     *     HTTPS certificate check entirely. Use with caution
+     *     -noKeyAuth   :
+     *     don't try to load the SSH authentication private key. Conflicts with
+     *     -i
+     *     
+     *     The available commands depend on the server. Run the 'help'
+     *     command to
+     *     see the list.}
+     */
+    public static Localizable _CLI_Usage() {
+        return new Localizable(holder, "CLI.Usage");
     }
 
 }
